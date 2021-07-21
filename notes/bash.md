a59f3028fe6d32f153f2e8dbd70d73dce43090cb
23wakeftanc <23wakeftanc@washk12.org>
2021-07-21 05:07:27 +0000

    eric fixed pip issues, it was a problem with setup.cfg; tanner wrote a lot of hangman and took notes

--- notes/bash.md
+++ notes/bash.md
@@ -3,8 +3,7 @@
 ```bash
 #display the message/print
 'echo "hi"'
-```
-```bash
+
 #lists contents of the directory you're in
 'ls'
 ```
@@ -18,24 +17,21 @@
 ```bash
 #go back to home or the root of the tree in linux
 '~'
-```
-```bash
+
 # / is the root of the tree
 '/'
 ```
 ```bash
 #where you are in a diectory
 '.'
-```
-```bash
+
 #go back one from where you are in a directory
 '..'
 ```
 ```bash
 # remove file, forever remove
 'rm'
-```
-```bash
+
 # move a file somewhere in the tree to a new location aka create in a new spot
 'mv'
 ```
@@ -46,4 +42,11 @@
 ```bash
 #print working directory, the full path to wherever you are on the tree
 'pwd'
+```
+```bash
+source venv/bin/activate
+```
+```bash
+#quit any running program
+ctrl+c
 ```
--- src/tannner_python/hangman.py
+++ src/tannner_python/hangman.py
@@ -1,4 +1,60 @@
+"""
+This file demo's the following principles
+functions:
+-def: defines a function
+-return: defines the function "call" should turn into (into what the function should "return")
+-"function call": whenever you run a function i.e. functio()
+-arguments/parameters: the variables that the function can take in, i.e. 'def function(a, b, c,)
+a, b c are the parameters
+-scope: local variables: variables that are defined in an indented block of code.
+They get deleted when the indented block finishes
+-global variables: variables that are defined outside of all the indented blocks. so they are in teh "global scope"
+any code block can see variables
+-docstrings: the first line of a function is called the dockstring. It should be a short summary of what the function does. 
+
+Variable types
+-variables: a value tht you can change. 'a=1' and then 'a=2' overwrites the 'a' variable.
+That's why it's called a variable, because we change its value over time.
+-constants: a variable that NEVER gets changed To show the reader that a variable is " constant",
+you make the constant name ALL_UPPERCASE_LIKE_THIS. if you do 'PI=3.1415' and then 'PI=3.14",
+ you are LYING to the people reading your code, becuase you changed a constant
+GLobal variables: if you need a variable in the global scope, PLEASE make it a constant so that users know to change it.
+ Otherwise it is a "global variables", those are DEATH. 90% of bugs in code come from global variables.
+
+ Typing:
+ there is a builtin library in python call "typing that helps you label what type of data you store in variables.
+ you declare a type like this: 'x: int = 5'. This shows that x should ALWAYS should always be an interger.
+ This is REALLY helpful for people for people reading your code, including you 2 weeks from now, when you have forgotten your
+ own code.
+
+ You can import 'List' and 'Union' from typing.
+
+ fruits: List[str] = ['apple', 'banana', 'orange']
+ that is how you d the "type annotation for a list of strings
+
+ numbers: List[Union[int,float,bool]] = [1,2,3,4.2,true]
+  is how you would do a type annotation for a list that can have intergers floats and booleans
+
+Good ways to get help
+-Google error messages and basic questions like "how do i check if a string contains a smaller string"
+-python commmand: 
+    -run 'python' in the terminal to get a "python shell" ( a terminal for python commands.)
+    You should see ">>>"
+    From there you can use the 'help' function and the type function
+        -help(something): you use it like this.
+            Say you want help with 'input' run help(input) to see the docstring of the function. super useful
+        -type(something): tells you what the type of something is. like 'type(5) will return int and type(True) will return bool
+        -exit(): this is how you quit the python shell
+- udemy course
+- stackoverfow
+- eric the cool man
+- ww3schools
+
+
+    
+"""
 from random import randint
+
 import sys
 from typing import List, Union
  #this is all caps becuase WORDS is a constant and the variable WORDS should never be changed
@@ -21,14 +77,20 @@ def show_help() -> None:
 def play_hangman():
     """Play a game of Hangman"""
     print("Welcome to Hangman!")
-    game_over = False
+    game_over:bool = False
     print("We're playing")
 
-    while not game_over:
-        WORDS=get_rand_word()
-        print(WORDS)
-        break
+    word:str=get_rand_word()
 
+    while not game_over:
+        word_length: int=len(word)
+        print("_ "*word_length)
+        guess:str=input("Guess a letter:")
+        guess_is_correct:bool= guess in word
+        if guess_is_correct: 
+            print("Correct!")
+        else:
+            print("Wrong :(")
 
 if __name__ == "__main__":
 # only play hangman if user ran "python hangman.py play"
